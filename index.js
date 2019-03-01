c3eb0617e2542329b6316fd07fc03580dad4de9d
Shawn Wilson <shawn.wilson137@gmail.com>
2019-03-01 19:45:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = [``]
+  for (let array = 1; array < 4; array++){
+    array[1] = `${musicians} plays ${instruments}`
+  }
+}
+
+function johnLennonFacts(){
+  
+}
+
+function iLoveTheBeatles(){
+  
+}
